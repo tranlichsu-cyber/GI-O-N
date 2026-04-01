ffa4711ea9ee18e6930326057a20e895fc3f4ecd
tranlichsu-cyber <tranlichsu@gmail.com>
2026-04-01 21:57:14 +0700

    feat(firebase): Add Firebase Analytics
    Integrates Firebase Analytics into the application for tracking user engagement and events. Includes a check to ensure analytics is only initialized if supported by the environment.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,12 +1,24 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCtWXx81QYrNtINq3xuQD8AD1Q9uFuhZ1s",
   authDomain: "tran-lich-su.firebaseapp.com",
   databaseURL: "https://tran-lich-su-default-rtdb.firebaseio.com",
-  projectId: "tran-lich-su"
+  projectId: "tran-lich-su",
+  storageBucket: "tran-lich-su.firebasestorage.app",
+  messagingSenderId: "1059341238849",
+  appId: "1:1059341238849:web:b757c4da780eca3a3d7cc0",
+  measurementId: "G-JGCENSS781"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+
+// Initialize Analytics if supported
+isSupported().then(yes => {
+  if (yes) getAnalytics(app);
+});
